067b4913db77849c68c14d722588372b62754034
Ubuntu <ubuntu@ip-172-31-92-153.ec2.internal>
2019-10-20 15:45:27 +0000

    interview

---
+++ ELB/terraform.tfvars
@@ -0,0 +1,2 @@
+AWS_ACCESS_KEY = "AKIAUVQ*******************"
+AWS_SECRET_KEY = "u4DhQwmsR****************************"
---
+++ windows/vars.tf
@@ -0,0 +1,19 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-0d4df21ffeb914d61"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
